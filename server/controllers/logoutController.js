4ee93fd1dcf4474d61497d38b5ec9f508cf8f1da
lupusalba <a145296@gmail.com>
2022-09-23 20:29:27 +0200

    removed console.log

--- server/controllers/logoutController.js
+++ server/controllers/logoutController.js
@@ -1,31 +1,32 @@
 const User = require('../Models/ModelUser')
 
-const handleLogout = async(req, res) => {
+const handleLogout = async (req, res) => {
   // on client also delete accessToken
 
-  const { cookies } =  req.cookies
-  if(!cookies?.jwt) return res.sendStatus(204)// no content
-  const refreshToken = cookies.jwt;
-
-  //check if refresh token is in DB
-  const foundUser = await User.findOne({refreshToken : refreshToken}).exec();
-  
-  if (!foundUser){
-    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-    return res.sendStatus(403)
+  const cookies = req.cookies;
+  if (!cookies?.jwt) {
+    console.log("no cookie");
+    return res.sendStatus(204)// no content
   }
-  
-  // delete refreshToken In DB
-  foundUser.refreshToken = '';
-  const result = await foundUser.save();
-  console.log(result);
+    const refreshToken = cookies.jwt;
 
-  res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-  res.sendStatus(204);
+    //check if refresh token is in DB
+    const foundUser = await User.findOne({ refreshToken: refreshToken }).exec();
+
+    if (!foundUser) {
+      res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+      return res.sendStatus(403)
+    }
+
+    // delete refreshToken In DB
+    foundUser.refreshToken = '';
+    const result = await foundUser.save();
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+    res.sendStatus(204);
 
 
-}
+  }
 
-module.exports = { handleLogout }
+  module.exports = { handleLogout }
 
 
